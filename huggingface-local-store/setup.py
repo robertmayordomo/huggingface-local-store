4d50e53df61bada1bf68c28cf9772b4ae20b719b
robertmayordomo <robert@mayordomo.co.uk>
2023-09-25 00:38:51 +0100

    added setup

---
+++ huggingface-local-store/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='huggingface-local-store',
+    version='0.0.1',
+    author='Robert Butler',
+    author_email='robert@mayordomo.co.uk',
+    description='Copy Huggingface models to the cloud',
+    long_description='Enable Copying Of Hugging face models to Azure Cloud.',
+    url='https://github.com/robertmayordomo/huggingface-local-store',
+    packages=find_packages(),
+    install_requires=[
+        'transformers>=4.30.2',
+        'azure-storage-blob>=12.18.1',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
